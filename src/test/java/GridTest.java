c3478e76cf722313e7066364ff374ed838258d58
Patrik Gustafsson <54358596+PG-87@users.noreply.github.com>
2020-10-26 10:38:43 +0100

    first test to check for only 2 values in array

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GridTest {
+
+    Grid grid;
+
+    @BeforeEach
+            void setup(){
+        grid = new Grid();
+    }
+
+    @Test
+    void createNewGridAndCheckForTwoDifferentCharacters(){
+        IntStream stream = Arrays.stream(grid.getGrid()).flatMapToInt(Arrays::stream);
+        assertEquals(2, stream.distinct().count());
+    }
 }
